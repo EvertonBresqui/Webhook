765e65041a9a340c0cee3083492e9de7501fcea3
Aluno <Aluno@LF103M011-69403>
2020-02-15 11:09:51 -0300

    primeiro commit

---
+++ Controllers/BotController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Protobuf;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebhookDF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BotController : ControllerBase
+    {
+        public string NomeAgente { get; set; }
+        private static readonly JsonParser _jsonParser = new JsonParser(JsonParser.Settings.Default.WithIgnoreUnknownFields(true));
+
+        System.Text.Json.JsonSerializerOptions _jsonSetting = new System.Text.Json.JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+
+        public BotController()
+        {
+            this.NomeAgente = "curso2-jaijhy";
+
+        }
+    }
+}
---
+++ Controllers/WebhookController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Cloud.Dialogflow.V2;
+using Google.Protobuf;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebhookDF.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WebhookController : ControllerBase
+    {
+		private ProjectAgentName _projectAgentName = new ProjectAgentName("curso2");
+
+		[HttpGet("[action]")]
+		public ActionResult DetectIntentFromTexts(string q, string sessionId)
+		{
+			try
+			{
+				var client = SessionsClient.Create();
+
+				DetectIntentRequest request = new DetectIntentRequest();
+				request.SessionAsSessionName = new SessionName(_projectAgentName.ProjectId, sessionId);
+				request.QueryInput = new QueryInput
+				{
+					Text = new TextInput()
+					{
+						Text = q,
+						LanguageCode = "pt-br"
+					}
+				};
+
+				DetectIntentResponse response = client.DetectIntent(request);
+
+				var queryResult = response.QueryResult;
+
+				return Ok(queryResult);
+
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, ex.Message);
+
+			}
+		}
+
+
+		[HttpGet("[action]")]
+		public ActionResult GetEvent(string eventName, string sessionId)
+		{
+
+			try
+			{
+				var client = SessionsClient.Create();
+
+				DetectIntentRequest request = new DetectIntentRequest();
+				request.SessionAsSessionName = new SessionName(_projectAgentName.ProjectId, sessionId);
+				request.QueryInput = new QueryInput
+				{
+					Event = new EventInput
+					{
+						Name = eventName,
+						LanguageCode = "pt-br"
+					}
+				};
+
+				request.QueryParams = new QueryParameters();
+
+				DetectIntentResponse response = client.DetectIntent(request);
+
+				var queryResult = response.QueryResult;
+
+				return Ok(queryResult);
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, ex.Message);
+			}
+
+		}
+
+
+	}
+}
+ 
